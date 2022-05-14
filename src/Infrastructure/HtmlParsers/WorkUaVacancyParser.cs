0813058f79bf3922ff16a7299703ac5f286f9b64
m.holtsev <DoctorMatt47@gmail.com>
2022-05-15 01:24:23 +0300

    Added not implemented work.ua vacancy parser

---
+++ src/Infrastructure/HtmlParsers/WorkUaVacancyParser.cs
@@ -0,0 +1,17 @@
+﻿using Application.Vacancies;
+using HtmlAgilityPack;
+using Infrastructure.Services;
+
+namespace Infrastructure.HtmlParsers;
+
+public class WorkUaVacancyParser : IVacancyParser
+{
+    private readonly IHtmlPageService _htmlPage;
+
+    public WorkUaVacancyParser(IHtmlPageService htmlPage) => _htmlPage = htmlPage;
+
+    public async Task<IEnumerable<GetVacancyResponse>> Get(GetVacanciesRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
