c14b39b7171bb551781f8369cec79824e6e251f1
m.holtsev <DoctorMatt47@gmail.com>
2022-05-15 20:09:38 +0300

    Added work ua vacancy parser

--- src/Infrastructure/HtmlParsers/WorkUaVacancyParser.cs
+++ src/Infrastructure/HtmlParsers/WorkUaVacancyParser.cs
@@ -6,12 +6,30 @@ namespace Infrastructure.HtmlParsers;
 
 public class WorkUaVacancyParser : IVacancyParser
 {
-    private readonly IHtmlPageService _htmlPage;
+    private readonly IDynamicPageService _dynamicPage;
 
-    public WorkUaVacancyParser(IHtmlPageService htmlPage) => _htmlPage = htmlPage;
+    public WorkUaVacancyParser(IDynamicPageService dynamicPage) => _dynamicPage = dynamicPage;
 
     public async Task<IEnumerable<GetVacancyResponse>> Get(GetVacanciesRequest request)
     {
-        throw new NotImplementedException();
+        var uri = $"https://www.work.ua/jobs-{request.City}-{request.Category}";
+        var html = await _dynamicPage.GetHtml(uri, null);
+
+        var document = new HtmlDocument();
+        document.LoadHtml(html);
+
+        return document.GetElementbyId("pjax-job-list").ChildNodes
+            .Where(node => node.HasClass("job-link"))
+            .Select(node =>
+            {
+                var companyAndCityNode = node.ChildNodes.First(n => n.HasClass("flex"));
+                return new
+                {
+                    Title = node.ChildNodes.First(n => n.OriginalName == "h2").ChildNodes[1].InnerText,
+                    CompanyName = companyAndCityNode.ChildNodes[1].InnerText,
+                    City = companyAndCityNode.ChildNodes[3].InnerText
+                };
+            })
+            .Select(v => new GetVacancyResponse(v.Title, v.CompanyName, string.Empty, string.Empty));
     }
 }
---
+++ tests/Infrastructure.IntegrationTests/HtmlParsers/WorkUaVacancyParserTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Vacancies;
+using Infrastructure.HtmlParsers;
+
+namespace Infrastructure.IntegrationTests.HtmlParsers;
+
+public class WorkUaVacancyParserTests
+{
+    private readonly IEnumerable<IVacancyParser> _parsers;
+
+    public WorkUaVacancyParserTests(IEnumerable<IVacancyParser> parsers) => _parsers = parsers;
+
+    [Fact]
+    public async Task GetParsedVacancies()
+    {
+        var parser = _parsers.First(p => p is WorkUaVacancyParser);
+
+        var vacancies = await parser.Get(new GetVacanciesRequest(".net", "киев"));
+
+        Assert.All(vacancies, vacancy =>
+        {
+            var (title, companyName, _, _) = vacancy;
+            
+            Assert.NotEqual(title, string.Empty);
+            Assert.NotEqual(companyName, string.Empty);
+        });
+    }
+}
