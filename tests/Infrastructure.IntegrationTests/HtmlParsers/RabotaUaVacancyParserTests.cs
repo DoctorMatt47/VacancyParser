9794855a24777c6d19907661347813114b26f59a
m.holtsev <DoctorMatt47@gmail.com>
2022-05-15 20:09:19 +0300

    Added rabota ua vacancy parser integration tests

---
+++ tests/Infrastructure.IntegrationTests/HtmlParsers/RabotaUaVacancyParserTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Vacancies;
+using Infrastructure.HtmlParsers;
+
+namespace Infrastructure.IntegrationTests.HtmlParsers;
+
+public class RabotaUaVacancyParserTests
+{
+    private readonly IEnumerable<IVacancyParser> _parsers;
+
+    public RabotaUaVacancyParserTests(IEnumerable<IVacancyParser> parsers) => _parsers = parsers;
+
+    [Fact]
+    public async Task GetParsedVacancies()
+    {
+        var parser = _parsers.First(p => p is RabotaUaVacancyParser);
+        
+        var vacancies = await parser.Get(new GetVacanciesRequest(".net", "киев"));
+        
+        Assert.All(vacancies, vacancy =>
+        {
+            var (title, companyName, _, _) = vacancy;
+            
+            Assert.NotEqual(title, string.Empty);
+            Assert.NotEqual(companyName, string.Empty);
+        });
+    }
+}
---
+++ tests/Infrastructure.IntegrationTests/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.IntegrationTests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Testing.json", true, true)
+            .Build();
+        
+        services.AddInfrastructure(configuration);
+    }
+}
