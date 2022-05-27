9692ef2094c38af2a17715c9f6579780101f8a2e
m.holtsev <DoctorMatt47@gmail.com>
2022-05-28 00:50:52 +0300

    Implemented entity framework configuration

---
+++ src/Infrastructure/Persistence/VacancyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VacancyParser.Domain.Entities;
+
+namespace VacancyParser.Infrastructure.Persistence;
+
+public class VacancyConfiguration : IEntityTypeConfiguration<Vacancy>
+{
+    public void Configure(EntityTypeBuilder<Vacancy> builder)
+    {
+        builder.HasKey(v => v.Link);
+    }
+}
---
+++ src/Infrastructure/Persistence/VacancyParserContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using VacancyParser.Application.Interfaces;
+using VacancyParser.Domain.Entities;
+
+namespace VacancyParser.Infrastructure.Persistence;
+
+public sealed class VacancyParserContext : DbContext, IVacancyParserContext
+{
+    private VacancyParserContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Vacancy> Vacancies { get; set; } = null!;
+
+    public Task SaveChangesAsync() => base.SaveChangesAsync();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
