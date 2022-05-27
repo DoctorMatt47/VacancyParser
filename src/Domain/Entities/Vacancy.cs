e60d4bdbb1d4ee650a9af1e86e955a7aeb42c4f3
m.holtsev <DoctorMatt47@gmail.com>
2022-05-28 00:51:00 +0300

    Added domain entity

---
+++ src/Domain/Entities/Vacancy.cs
@@ -0,0 +1,27 @@
+﻿namespace VacancyParser.Domain.Entities;
+
+public class Vacancy
+{
+    public Vacancy(
+        string link,
+        string title,
+        string companyName,
+        string salary,
+        string city,
+        string category)
+    {
+        Link = link;
+        Title = title;
+        CompanyName = companyName;
+        Salary = salary;
+        City = city;
+        Category = category;
+    }
+
+    public string Link { get; protected set; }
+    public string Title { get; protected set; }
+    public string CompanyName { get; protected set; }
+    public string Salary { get; protected set; }
+    public string City { get; protected set; }
+    public string Category { get; protected set; }
+}
